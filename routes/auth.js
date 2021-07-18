914007481d0c676be4d27b9c90acdb8422fcb6cb
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-18 20:18:11 +0800

    Added user validation using Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Joi = require("Joi");
+
+const schema = {
+  name: Joi.string().min(6).required(),
+  email: Joi.string().email().min(6).email(),
+  password: Joi.string().min(6).required(),
+};
 
 router.post("/register", async (req, res) => {
+  // validate user data
+
+  const validation = schema.validate(req.body);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
