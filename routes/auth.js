f82b5b7cbd9e9c7f17d3d998c9ee0014b262633e
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-18 21:43:14 +0800

    Added password hashing functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
@@ -10,10 +11,14 @@ router.post("/register", async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send("Email already exists");
 
+  // hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
