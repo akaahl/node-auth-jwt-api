3cc179389c9618f9b58c263e73a00779cde835a6
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-18 21:26:44 +0800

    Added repetition validation

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,11 @@ const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
-
   if (error) return res.status(400).send(error.details[0].message);
+
+  // check if the user's email alread exist in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
 
   const user = new User({
     name: req.body.name,
