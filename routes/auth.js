77fa8bb5ccbf07a0ed150f8147dd3fe48d34d6d5
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-19 17:04:52 +0800

    Added loginValidation process

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const { registerValidation } = require("../validation");
+const { registerValidation, loginValidation } = require("../validation");
 
+// registering user
 router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -23,10 +24,26 @@ router.post("/register", async (req, res) => {
 
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch (err) {
     console.log(err);
   }
+});
+
+// logging user in
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // check if email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email is not found");
+
+  // password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  res.send("logged in!");
 });
 
 module.exports = router;
