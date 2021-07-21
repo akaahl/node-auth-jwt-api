f957e0619d8508225b4f20976000d351a5a689b0
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-21 17:07:37 +0800

    Added pre-saved function on the schema that encrypts and hashes the password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter a password"],
     minLength: [6, "Password must be atleast 6 characters long."],
   },
+});
+
+// fire a function after the user has been saved to db
+userSchema.post("save", function (doc, next) {
+  console.log("A new user has been created", doc);
+  next();
+});
+
+// fire a function before a user is saved to db
+userSchema.pre("save", async function (next) {
+  console.log("A user is about to be created and saved", this);
+
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
